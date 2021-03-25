35358da13adc238bd89ffaefbf786fd3453d9264
Teddy <jfrancoisteddy@gmail.com>
2021-03-25 22:52:09 +0100

    Function add a country to the ledger

--- contracts/src/contracts/country.contract.ts
+++ contracts/src/contracts/country.contract.ts
@@ -1,77 +1,30 @@
 import {Context, Contract} from "fabric-contract-api";
-import { Country } from "../models/country.model";
+import {Country, ICountryProps} from "../models/country.model";
 
 const chalk = require('chalk');
 const SHA256  = require('crypto-js/sha256');
 
 export class CountryContract extends Contract {
 
+
     /**
      *
-     * initLedger
+     * addCountry
      *
-     * Ledger initialization with first countries
+     * Add a country to the ledger
      *
      * @param ctx
+     * @param props - Country object
      */
-    public async  initLedger(ctx: Context) {
-        console.info(chalk.blue('============= START : Initialize Ledger ==========='));
-        const countries: Country[] = [
-            {
-                code: "FR",
-                name: "France",
-                populationSize: 66_000_000,
-                doseStorage: [
-                    {
-                        name: "Pfizer",
-                        doses: 50_000_000
-                    },
-                    {
-                        name: "AstraZeneca",
-                        doses: 44_000_000
-                    }
-                ],
-                vaccinationPoint: [
-                    {
-                        name: "Centre Hospitalier de Saint-Denis",
-                        address: "2 Rue du Dr Delafontaine",
-                        postalCode: "93200",
-                        city: "Saint-Denis",
-                        doses: 51_000
-                    },
-                ]
-            },
-        ];
-
-        countries.forEach(function (country, index) {
-            // Assign random id in get the number of miliseconds
-            countries[index].id = SHA256(Math.floor(Math.random() * Date.now()));
-            // It contains a key and value which needs to be written to the transaction's write set.
-            await ctx.stub.putState('COUNTRY' + index, Buffer.from(JSON.stringify(countries[index])));
-            console.info('Added <--> ', countries[index]);
-        });
-        console.info(chalk.blue('============= END : Initialize Ledger ==========='));
-    }
-
-    public async addCountry() {
+    public async addCountry(ctx: Context, props: ICountryProps) {
         console.info(chalk.green('============= START : Create Country ==========='));
 
+        const randomId = SHA256(Math.floor(Math.random() * Date.now()));
+        await ctx.stub.putState(randomId, Buffer.from(JSON.stringify(props)));
 
         console.info(chalk.green('============= END : Create Country ==========='));
     }
 
-    /**
-     *
-     * queryCountry
-     *
-     * Get country from chaincode state
-     *
-     * @param ctx
-     * @returns - country object converted in string
-     */
-    public async queryCountry(ctx: Context, ): Promise<string> {
 
-        return "";
-    }
 
 }
