60177fb8062767edcb4751e947e6f9e333dc4d5d
Teddy <jfrancoisteddy@gmail.com>
2021-03-25 22:53:10 +0100

    Function ledger initialization with first countries

--- contracts/src/contracts/country.contract.ts
+++ contracts/src/contracts/country.contract.ts
@@ -6,6 +6,54 @@ const SHA256  = require('crypto-js/sha256');
 
 export class CountryContract extends Contract {
 
+    /**
+     *
+     * initLedger
+     *
+     * Ledger initialization with first countries
+     *
+     * @param ctx
+     */
+    public async  initLedger(ctx: Context) {
+        console.info(chalk.blue('============= START : Initialize Ledger ==========='));
+
+        const countries: Country[] = [
+            {
+                code: "FR",
+                name: "France",
+                populationSize: 66_000_000,
+                doseStorage: [
+                    {
+                        name: "Pfizer",
+                        doses: 50_000_000
+                    },
+                    {
+                        name: "AstraZeneca",
+                        doses: 44_000_000
+                    }
+                ],
+                vaccinationPoint: [
+                    {
+                        name: "Centre Hospitalier de Saint-Denis",
+                        address: "2 Rue du Dr Delafontaine",
+                        postalCode: "93200",
+                        city: "Saint-Denis",
+                        doses: 51_000
+                    },
+                ]
+            },
+        ];
+
+        countries.forEach(function (country, index) {
+            // Assign random id by using sha256 hash function
+            countries[index].id = SHA256(Math.floor(Math.random() * Date.now()));
+            // It contains a key and value which needs to be written to the transaction's write set.
+            ctx.stub.putState('COUNTRY' + index, Buffer.from(JSON.stringify(countries[index])));
+            console.info('Added <--> ', countries[index]);
+        });
+
+        console.info(chalk.blue('============= END : Initialize Ledger ==========='));
+    }
 
     /**
      *
