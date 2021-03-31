12d89e92950e93d485d38d83fcdc1a4251e544c8
Teddy <jfrancoisteddy@gmail.com>
2021-03-31 16:44:57 +0200

    Add new classes models

--- chaincode/models/citizen.model.ts
+++ chaincode/models/citizen.model.ts
@@ -3,9 +3,25 @@
  */
 
 export interface ICitizenProps {
-    
+    id?: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    gender: string;
+    register?: boolean;
+    vaccine?: Array<object>;
 }
 
 export class Citizen implements ICitizenProps {
-
+    firstName: string;
+    lastName: string;
+    age: number;
+    gender: string;
+    
+    constructor(props :ICitizenProps) {
+        this.firstName = props.firstName,
+        this.lastName = props.lastName,
+        this.age = props.age,
+        this.gender = props.gender
+    }
 }
--- chaincode/models/doctor.model.ts
+++ chaincode/models/doctor.model.ts
@@ -2,10 +2,26 @@
  * Doctor class
  */
 
-export interface IDoctorProps {
+import { ICitizenProps } from "./citizen.model";
 
+export interface IDoctorProps extends ICitizenProps {
 }
 
-export class Doctor implements IDoctorProps {
+export class Doctor implements IDoctorProps{
+    id?: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    gender: string;
+    register?: boolean;
+    vaccine?: Array<object>;
+    disponibilityStart?: string;
+    disponibilityEnd?: string;
 
+    constructor(props :IDoctorProps) {
+        this.firstName = props.firstName,
+        this.lastName = props.lastName,
+        this.age = props.age,
+        this.gender = props.gender
+    }
 }
