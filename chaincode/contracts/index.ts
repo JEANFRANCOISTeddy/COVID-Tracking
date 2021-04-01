87975d012bef7bf1c557bf19948fbe3aa9a30a28
Teddy <jfrancoisteddy@gmail.com>
2021-04-01 15:54:41 +0200

    remove laboratory contract

--- chaincode/contracts/index.ts
+++ chaincode/contracts/index.ts
@@ -2,4 +2,3 @@ export * from './country.contract';
 export * from './doctor.contract';
 export * from './citizen.contract';
 export * from './vaccinationPoint.contract';
-export * from './laboratory.contract';
