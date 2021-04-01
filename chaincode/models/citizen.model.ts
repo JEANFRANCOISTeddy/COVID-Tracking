72489694f61dfc87d6b70af6059659b04743b5d5
Teddy <jfrancoisteddy@gmail.com>
2021-04-01 15:46:30 +0200

    Correction citizen model

--- chaincode/models/citizen.model.ts
+++ chaincode/models/citizen.model.ts
@@ -3,25 +3,32 @@
  */
 
 export interface ICitizenProps {
-    id?: string;
+    socialSecurityCardId: string; // check if your a citizen in using numbers on your social security card
     firstName: string;
     lastName: string;
     age: number;
     gender: string;
+    nationality: string;
+    covidResult?: boolean;
     register?: boolean;
     vaccine?: Array<object>;
+    vaccinationAvailability?: string; 
 }
 
 export class Citizen implements ICitizenProps {
+    socialSecurityCardId: string;
     firstName: string;
     lastName: string;
     age: number;
     gender: string;
+    nationality: string;
     
     constructor(props :ICitizenProps) {
+        this.socialSecurityCardId = props.socialSecurityCardId,
         this.firstName = props.firstName,
         this.lastName = props.lastName,
         this.age = props.age,
-        this.gender = props.gender
+        this.gender = props.gender,
+        this.nationality = props.nationality
     }
 }
